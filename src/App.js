0044fc7d4e87a88394bb0bb10aa5374fc3d68a80
murshidp <murshidparambadan@gmail.com>
2020-03-18 06:47:48 +0530

    style added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css'
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <input type="checkbox"></input>
       <p>Placeholder text here</p>
       <input type='checkbox'></input>
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react"
+import "../style.css"
+
+function Header(){
+    return <header className="navbar">ToDo App</header>
+}
+export default Header
