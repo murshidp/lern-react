887af4c86560540690a8f50f786cd04567cf0b30
murshidp <murshidparambadan@gmail.com>
2020-03-18 05:41:59 +0530

    to do app phase1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import Footer from "./Footer"
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <h1>hello third time</h1>
-        <ul>Thing 1</ul>
-        <ul>Thing 2</ul>
-        <ul>Thing 3</ul>
-      </nav>
-      <main>This is were most of my content go</main>
-      <Footer />
+      <input type="checkbox"></input>
+      <p>Placeholder text here</p>
+      <input type='checkbox'></input>
+      <p>Place holder text here</p>
     </div>
   );
 }
