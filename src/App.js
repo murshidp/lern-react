4853e1f4e4e5576291eac4af4c9c3c6dfc6da58c
murshidp <murshidparambadan@gmail.com>
2020-03-22 12:02:57 +0530

    start lerning props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import './App.css'
 import Header from "./components/Header"
-import ToDoItem from "./components/ToDoItem"
+import ContactCard from "./components/ContactCard"
 
 function App() {
   return (
-    <div className="todo-list">
-      <Header/>
-      <ToDoItem/>
-      <ToDoItem/>
-      <ToDoItem/>
+    <div className="contact-card">
+     <ContactCard
+        name="murshid"
+        imgUrl="http://placekitten.com/300/200"
+        phone="9977665544"
+        email="msd@g.com"
+     />
+     <ContactCard
+      name="adul"
+      imgUrl="http://placekitten.com/400/200"
+      phone="887665432"
+      email="adul@gmail.com"
+     />
+     <ContactCard 
+      name="asik"
+      imgUrl="http://placekitten.com/300/300"
+      phone="9999999999"
+      email="ashik@gmail.com"
+     />
+     <ContactCard 
+     name="jash"
+     imgUrl="http://placekitten.com/200/100"
+     phone="9966554433"
+     email="js@gmail.com"
+     />
     </div>
   );
 }
