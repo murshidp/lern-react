72fdf6a7b2d57a6da837870f9f7afbaf7e749764
murshidp <murshidparambadan@gmail.com>
2020-03-22 12:14:44 +0530

    prop another method

--- src/App.js
+++ src/App.js
@@ -7,28 +7,27 @@ function App() {
   return (
     <div className="contact-card">
      <ContactCard
-        name="murshid"
-        imgUrl="http://placekitten.com/300/200"
-        phone="9977665544"
-        email="msd@g.com"
+     contact = {{name:"murshid",imgUrl:"http://placekitten.com/300/200",
+     phone:"9977665544",email:"msd@g.com"}}
+      
      />
      <ContactCard
-      name="adul"
-      imgUrl="http://placekitten.com/400/200"
-      phone="887665432"
-      email="adul@gmail.com"
+      contact = {{name:"adul",
+      imgUrl:"http://placekitten.com/400/200",
+      phone:"887665432",
+      email:"adul@gmail.com"}}
      />
      <ContactCard 
-      name="asik"
-      imgUrl="http://placekitten.com/300/300"
-      phone="9999999999"
-      email="ashik@gmail.com"
+      contact ={{name:"asik",
+      imgUrl:"http://placekitten.com/300/300",
+      phone:"9999999999",
+      email:"ashik@gmail.com"}}
      />
      <ContactCard 
-     name="jash"
-     imgUrl="http://placekitten.com/200/100"
-     phone="9966554433"
-     email="js@gmail.com"
+     contact={{name:"jash",
+     imgUrl:"http://placekitten.com/200/100",
+     phone:"9966554433",
+     email:"js@gmail.com"}}
      />
     </div>
   );
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -3,10 +3,10 @@ import React from "react"
 function ContactCard(props){
     return(
         <div className="Contact-card">
-            <img src={props.imgUrl}/>
-            <h3>{props.name}</h3>
-            <p>{props.phone}</p>
-            <p>{props.email}</p>
+            <img src={props.contact.imgUrl}/>
+            <h3>{props.contact.name}</h3>
+            <p>{props.contact.phone}</p>
+            <p>{props.contact.email}</p>
         </div>
     )
 }
