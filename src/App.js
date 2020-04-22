76cbd0970603d6db99524c693562059127999678
murshidp <murshidparambadan@gmail.com>
2020-04-22 07:16:06 +0530

    added event in console

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css'
-import Header from "./components/Header"
 import ToDoItem from "./components/ToDoItem"
-import TodoData from "./components/ToDoDatas"
 import todoData from './components/ToDoDatas';
 
 class App extends React.Component {
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from "react"
 function ToDoItem(props) {
     return(
         <div className="todo-item">
-        <input type="checkbox" checked={props.item.completed}></input>
+        <input type="checkbox" checked={props.item.completed} onChange={()=>console.log("changed")}></input>
       <p>{props.item.text}</p>
     </div>
     )
