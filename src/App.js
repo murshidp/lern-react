ae65fcaa555c514175c602e7bb84231a578a4386
murshidp <murshidparambadan@gmail.com>
2020-04-22 06:48:29 +0530

    changed in to a class based app

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css'
 import Header from "./components/Header"
 import ToDoItem from "./components/ToDoItem"
 import TodoData from "./components/ToDoDatas"
+import todoData from './components/ToDoDatas';
 
-function App() {
-  const todoItems = TodoData.map(item => <ToDoItem key={item.id} item={item}/>)
-  return (
-    <div className="todo-list">
-      {todoItems}
-     
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      todos: todoData
+    }
+  }
+  render(){
+    const todoItems = this.state.todos.map(item => <ToDoItem key={item.id} item={item}/>)
+    return (
+      <div className="todo-list">
+        {todoItems}
+       
+      </div>
+    );
+  }
+  }
+ 
 
 export default App;
