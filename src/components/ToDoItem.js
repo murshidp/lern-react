6efaf3fb79e06a05451e4b2e76b73edc2975edec
murshidp <murshidparambadan@gmail.com>
2020-04-27 08:30:56 +0530

    small styling

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,10 +1,15 @@
 import React from "react"
 
 function ToDoItem(props) {
+  const completedStyle = {
+    fontStyle:"italic",
+    color:"red",
+    textDecoration:"line-through"
+  }
     return(
         <div className="todo-item">
         <input type="checkbox" checked={props.item.completed} onChange={()=>props.handleChange(props.item.id)}></input>
-      <p>{props.item.text}</p>
+      <p style={props.item.completed ? completedStyle : null}>{props.item.text}</p>
     </div>
     )
    
